c8e7bdba86b81a813b379d52544067c77a0c3ced
cyrilvincent <contact@cyrilvincent.com>
2018-08-30 11:31:29 +0200

    UnitTest

---
+++ FormationPOENET/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormationPOEWebAPI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            int i = 1;
+            i += 1;
+            Assert.AreEqual(2, i);
+            Book b = new Book();
+
+        }
+
+        [TestMethod]
+        public void EFTest()
+        {
+            Model1 db = new Model1();
+            List<Book> books = db.Books.ToList();
+            Assert.IsTrue(books.Count > 0);
+        }
+    }
+}
